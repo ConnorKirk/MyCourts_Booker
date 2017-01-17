d988a89923a68726a974e1dea3ef8cc385b1f173
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2017-01-17 08:36:44 +0000

    Create functions to get ctid.
    Start creation of function to assemble hyperlink to book court.

--- Main.R
+++ Main.R
@@ -1,34 +1,33 @@
+#Libraries
+library(stringr)
 library(httr)
 library(rvest)
 library(dplyr)
 library(magrittr)
+library(lubridate)
 
-
-
-
-courts <- list(court1 = "court563",
-               court2 = "court564",
-               court3 = "court565",
-               court4 = "court566")
+#Load username and password
+source("credentials.R")
 
 #Goes to edgbaston mycourts page (current day)
 mycourts_url <- "https://edgbastonpriory.mycourts.co.uk/" 
 
 
-source("credentials.R")
-
-#login
+#Create new session
 sesh <- html_session(mycourts_url)
 
+#read session html
 h <- sesh %>%
   read_html() 
 
+#Get login form
 h_form <- html_form(h)[[1]]
 
+#Set login form credentials
 h_form_values_set <-  set_values(h_form,
                                  username = username,
                                  password = password)
-
+#Submit login form
 h_form_submit <- submit_form(form = h_form_values_set,
                              session = sesh,
                              Submit = "Submit")
@@ -36,32 +35,49 @@ h_form_submit <- submit_form(form = h_form_values_set,
 
 # Navigate to Correct Date
 sesh <- sesh %>%
-  jump_to("https://edgbastonpriory.mycourts.co.uk/bookings.asp?st1=1600&st2=2400&d=14&tabID=132")
+  jump_to("https://edgbastonpriory.mycourts.co.uk/bookings.asp?st1=0700&st2=2400&d=14&tabID=132")
 
-#Log in Works
 
-read_html(sesh) %>%
-  html_nodes("#left_col a")
+
+# #Log in Works
+# #Check log in successfull
+#  read_html(sesh) %>%
+#    html_nodes("#left_col a")
   
-booking_link <- sesh %>%
+
+# #Find link for "booking" court
+# booking_link <- sesh %>%
+#   read_html() %>%
+#   html_node("#court563 .book_this_court") %>%
+#   html_attr("href") %>%
+#   paste0(mycourts_url, .) %>%
+#   stringr::str_replace_all(pattern = " ", replacement = "%20")
+
+
+#Follow link to book court
+# sesh <- sesh %>%
+#   jump_to(booking_link)
+
+
+#Find link to confirm court booking
+# confirmation_link <- sesh %>%
+#   read_html() %>%
+#   html_node(css = "#right_col > table > tr:nth-child(2) > td:nth-child(4) > a") %>%
+#   html_attr("href") %>%
+#   paste0(mycourts_url, .) %>%
+#   stringr::str_replace_all(pattern = " ", replacement = "%20")
+ 
+#Follow link to confirm court booking 
+# sesh <- sesh %>%
+#   jump_to(confirmation_link)
+
+#Gets the court times from sesh
+sesh %>%
   read_html() %>%
-  html_node("#court563 .book_this_court") %>%
-  html_attr("href") %>%
-  paste0(mycourts_url, .) %>%
-  stringr::str_replace_all(pattern = " ", replacement = "%20")
+  html_nodes("#court566 > div") %>%
+  html_text()
 
 
 
-sesh <- sesh %>%
-  jump_to(booking_link)
 
-confirmation_link <- sesh %>%
-  read_html() %>%
-  html_node(css = "#right_col > table > tr:nth-child(2) > td:nth-child(4) > a") %>%
-  html_attr("href") %>%
-  paste0(mycourts_url, .) %>%
-  stringr::str_replace_all(pattern = " ", replacement = "%20")
-  
-sesh <- sesh %>%
-  jump_to(confirmation_link)
   
--- court_selector.R
+++ court_selector.R
@@ -1,16 +1,55 @@
 #url builder
-library(lubridate)
-selection <-  list(time = 1720, court = 1, date = "26 January 2017")
 
-base_url_book <-  "https://edgbastonpriory.mycourts.co.uk//bookings_confirm.asp?"
-base_url_confirm <- "https://edgbastonpriory.mycourts.co.uk//bookings_process.asp?"
-ctid <-  "14383"
-day <- "20"
-month <- "January"
-year <- "2017"
+#Maps Courts for Court number. Could substitute for simple function court# + 562?
+courts <- list(court1 = "563",
+               court2 = "564",
+               court3 = "565",
+               court4 = "566",
+               court5 = "567",
+               court6 = "568",
+               court7 = "569",
+               court8 = "570",
+               court9 = "571",
+               court10 = "572")
 
-query <-  paste0(base_url, "ctid=", ctid, "&dt=", selection$date)
+#Extracts which link has required court from given time. 
+#Could be combined with below into a function
+court_link_selector <- function(session, court_number, court_time ) {
+  session %>%
+    read_html() %>%
+    html_nodes(paste0("#court", court_number, " div")) %>% #creates selector ie "#court566 div"
+    stringr::str_detect(paste0(court_time, ":")) %>%
+    which()
+}
 
-days <- dmy(selection$date[[1]]) - today() %>%
-  as.numeric()
+#Extracts CourtId from given court and link_no
+ctid_finder <- function(session, link_no, court_number) {
+  session %>%
+    read_html() %>%
+    html_nodes(paste0("#court", court_number, " div")) %>%
+    extract2(link_no) %>%
+    html_node("a") %>%
+    html_attr("href") %>%
+    stringr::str_extract("(?<=ctid=)\\d\\d\\d\\d")
+}  
+
+create_confirmation_link <- function(session, court_number, court_time, date) {
+  link_no <-  court_link_selector(session = session, court_number = court_number, court_time = court_time)
+  
+  ctid <- ctid_finder(session = session, link_no = link_no, court_number = court_number)
+  
+  
+  base_url_confirm <- "https://edgbastonpriory.mycourts.co.uk//bookings_process.asp?"
+  confirmation_link <-  paste0(base_url_confirm, "ctid=", ctid, "&dt=", booking_date)
+}
+
+
+
+
+
+
+
+
+ 
+  
 
